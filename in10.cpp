b9c17dc888ff694d0fca88bf2867fa0b02277790
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-09-24 15:11:16 +0300

    Update in10.cpp

--- in10.cpp
+++ in10.cpp
@@ -7,9 +7,9 @@ int main()
     int a,b;
     cout<<"Число ";
     cin >> a;
-    b=a*a; //2-я степень
-    a=b*b; //4-я
-    a=a*a; //8-я
+    b=a*a; //2
+    a=b*b; //4
+    a=a*a; //8
     a=a*b; //10
     cout << a;
     return 0;
