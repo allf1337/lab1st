e915dffc45628f28655e3227e863e96b330b1aaf
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-09-24 15:02:13 +0300

    Update in8.cpp

--- in8.cpp
+++ in8.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main()
 {
-    int a,b,c,d,e;
+    int a;
     cout<<"Число ";
     cin >> a;
     a=a*a; //2-я степень
     a=a*a; //4-я
     a=a*a; //8-я
+    cout <<a;
     return 0;
 }
