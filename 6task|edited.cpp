639f39e22f51447a85634e3386343d0199402ea1
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-09-29 19:25:17 +0300

    Update 6task|edited.cpp

--- 6task|edited.cpp
+++ 6task|edited.cpp
@@ -11,22 +11,22 @@ int main()
 	std::cout <<"x1= ";
 	std::cin >> x1;
 	if (std::cin.fail()) 
-		{std::cout << "Некорректное значение! \n";
+		{std::cout << "Некорректное значение x1! \n";
 		return 0;}
 	std::cout <<"y1= ";
 	std::cin >> y1;
 	if (std::cin.fail()) 
-  	{std::cout << "Некорректное значение! \n";
+  	{std::cout << "Некорректное значение y1! \n";
   	return 0;}
 	std::cout <<"x2= ";
 	std::cin >> x2;
 	if (std::cin.fail()) 
-  	{std::cout << "Некорректное значение! \n";
+  	{std::cout << "Некорректное значение x2! \n";
   	return 0;}
 	std::cout <<"y2= ";
 	std::cin >> y2;
 	if (std::cin.fail()) 
-  	{std::cout << "Некорректное значение! \n";
+  	{std::cout << "Некорректное значение y2! \n";
   	return 0;}
 	if (x1!=x2) h=x2-x1;
 	    else h=y2-y1;
