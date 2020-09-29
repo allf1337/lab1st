9accac453ad26063b22c3e614d49b4067e2343d5
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-09-29 19:10:47 +0300

    Rename 6task.cpp/edited to 6task|edited.cpp

--- 6task|edited.cpp
+++ 6task|edited.cpp
@@ -8,14 +8,14 @@ int main()
 {
 	setlocale(LC_ALL, "rus");
 	int x1,y1,x2,y2,h,S;
-	cout <<"x1= ";
-	cin >> x1;
-	cout <<"y1= ";
-	cin >> y1;
-	cout <<"x2= ";
-	cin >> x2;
-	cout <<"y2= ";
-	cin >> y2;
+	std::cout <<"x1= ";
+	std::cin >> x1;
+	std::cout <<"y1= ";
+	std::cin >> y1;
+	std::cout <<"x2= ";
+	std::cin >> x2;
+	std::cout <<"y2= ";
+	std::cin >> y2;
 	if (x1!=x2) h=x2-x1;
 	    else h=y2-y1;
 	S = h*h*6;
