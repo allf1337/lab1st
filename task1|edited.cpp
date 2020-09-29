2a5c824415ad3ea94c9876d30e5c312016ab85ad
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-09-29 21:01:46 +0300

    Update task1|edited.cpp

--- task1|edited.cpp
+++ task1|edited.cpp
@@ -8,15 +8,15 @@ int main()
 	setlocale(LC_ALL, "Russian");
 
 	int R, r, S;
-	std::cout << "Введите внешний радиус кольца R:";
+	std::cout << "Введите внешний радиус кольца R: \n";
 	std::cin >> R;
 		if (R < 0 || (std::cin.fail())) {
-			cout << "Некорректное значение R";
+			cout << "Некорректное значение R \n";
 				return 0; }
-	std::cout << "Введите внутренний радиус кольца r:";
+	std::cout << "Введите внутренний радиус кольца r: \n";
 	std::cin >> r;
 		if (r < 0 || (std::cin.fail())) {
-			cout << "Некорректное значение r";
+			cout << "Некорректное значение r \n";
 				return 0; }
 	S = M_PI *(pow(R,2) - pow(r,2));
 	std::cout << "Площадь кольца равна: " << S << endl;
