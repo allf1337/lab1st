5d5f8f09d807cf46ff86ba666eac1d969cf9400f
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-10-15 01:17:21 +0300

    Update and rename in13.cpp to lab5th/7th.cpp

---
+++ lab5th/7th.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <locale.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+
+
+using namespace std;
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+double x_start, x_end, y1, y2, y3, x_d, a, b, k;
+cout << "Введите X начальное: ";
+cin >> x_start;
+cout << "Введите X конечное: ";
+cin >> x_end;
+cout<< "Введите шаг dX: ";
+cin >>x_d;
+cout<< "Введите а: ";
+cin >> a;
+cout <<"Введите b: ";
+cin >> b;
+if (x_start > x_end)
+{
+k = x_start;
+x_start = x_end;
+x_end = k;
+}
+cout << " X | Y1 | Y2 | Y3 |" << endl;
+cout << "-----------------------------------------------------------------" << endl;
+while (x_start <= x_end)
+{
+if (x_start <= 0)
+y1 = sqrt(a * a - pow((x_start + a), 2));
+else y1 = -b * sin(x_start);
+if (x_start <= -1)
+y2 = log10(b / (x_start * x_start));
+else y2 = log10(b + sqrt(x_start + 1));
+if (x_start <= 0)
+y3 = fabs(x_start * x_start + 3.14 * x_start + 1);
+else y3 = 1 + b * log(1 + x_start);
+cout.width(8); cout.precision(2);
+cout << x_start << " | ";
+cout.width(8); cout.precision(2);
+cout<< y1 << " | ";
+cout.width(8); cout.precision(2);
+cout << y2 << " | ";
+cout.width(8); cout.precision(2);
+cout << y3 << " | ";
+x_start = x_start + x_d;
+}
+cin.get();
+system("pause");
+return 0;
+}
