6ba31759e9c354eb62074f95acf9d67a0cc5bc02
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-10-15 12:09:01 +0300

    Update task1st.cpp

--- lab6th/task1st.cpp
+++ lab6th/task1st.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
+#include <locale.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+
 
 using namespace std;
 
-int main()
-{
-    int a,b,c,d,e;
-    cout<<"Число ";
-    cin >> a;
-    b=a*a; //2
-    c=b*a; //3
-    d=c*c; //6
-    e=d*d; //12
-    a=c*e; //15
-    cout << a;
-    return 0;
+
+int main() {
+  int const n=10;
+  double S[n], proizv=1;
+  cout << "ddsds" << endl;
+  for (int i=0; i<n; i++)
+  {
+    cin>>S[i];
+  }
+  for (int i=0; i<n; i++)
+  {
+    if (i%2==0)
+    {
+      //proizv=S[i]*proizv;
+      proizv=S[i]*proizv*proizv;
+    }
+  }
+cout << "Proizv: " << proizv << endl;
+
 }
+
