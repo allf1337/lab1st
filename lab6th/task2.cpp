4c89f3dcea75246842d43b4b21447c9e938068e2
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-10-15 12:47:45 +0300

    Update task2.cpp

--- lab6th/task2.cpp
+++ lab6th/task2.cpp
@@ -40,3 +40,48 @@ int main() {
 }
 
 
+
+
+
+
+
+
+#include <iostream>
+#include <locale.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+
+
+using namespace std;
+
+
+int main() {
+  int  k = 0, s = 0, p = 0;
+  int const n=10;
+  double S[n], proizv=1;
+  cout << "Введите элементы массива: " << endl;
+  for (int i=0; i<n; i++)
+  {
+    cin>>S[i];
+  }
+  for (int i=0; i<n; i++) 
+    if (S[i]==0)
+    {
+      k=1;
+      break;
+    }
+    for (int i=n; i<n; i--)
+    if (S[i]==0)
+    {
+      p=i;
+    }
+    for (int i=k; i<p; i++)
+    {
+      s+=S[i]+s;
+    
+    }
+  cout << "Sum: " << s << endl;
+    
+}
+
