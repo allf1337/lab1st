b5810125834da4f44108c917a14b92deba176374
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-11-20 11:58:25 +0300

    Update 4th.cpp

--- lab7th/4th.cpp
+++ lab7th/4th.cpp
@@ -53,3 +53,56 @@ int main()
  
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int size;
+    cin>>size;
+    int** a=new int*[size];
+    for(int i=0; i<size;i++) {
+        a[i]=new int[size];
+    }
+    int** b=new int*[size];
+    for(int i=0; i<size;i++) {
+        b[i]=new int[size];
+    }
+    for (int i=0;i<size;i++){
+        cout<<endl;
+        for (int j=0;j<size;j++){
+            a[i][j] = 10 + rand()%89;
+            cout<<a[i][j]<<" ";
+        }
+    }
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            b[i][size-1-j] = a[j][i];
+        }
+    }
+    cout<<endl<<"++++++++";
+    for(int i=0;i<size;i++){
+        cout<<endl;
+        for(int j=0;j<size;j++)
+                cout<<b[i][j]<<" ";
+    }
+    for(int i=0; i<size;i++) {
+        delete [] a[i];
+    }
+    delete [] a;
+    for(int i=0; i<size;i++) {
+        delete [] b[i];
+    }
+    delete [] b;
+    return 0;
+}
