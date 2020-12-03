6a5aa42e3500bf124e8c2a87b89ed974cbe730a0
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-12-03 16:49:41 +0300

    Create 1th.cpp

---
+++ lab8th/1th.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <locale.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <algorithm>
+ 
+using namespace std;
+
+int main() {
+ int n, i,s;
+ int *a = new int [100];
+    cout << "Кол-во элементов массива (n<=100): ";
+    cin >> n;
+    cout <<"Массив: " << endl;
+    for(i = 0; i < n; i++) 
+    {
+        cout << "a[ " << i+1 << " ] = ";
+        cin >> a[i];
+    }
+
+	for (int startIndex = 0; startIndex < n - 1; ++startIndex)
+	{
+	
+		int smallestIndex = startIndex;
+ 
+		
+		for (int currentIndex = startIndex + 1; currentIndex < n; ++currentIndex)
+		{
+			
+			if (a[currentIndex] < a[smallestIndex])
+			
+				smallestIndex = currentIndex;
+		}
+ 
+		
+		std::swap(a[startIndex], a[smallestIndex]);
+	}
+ 
+	
+	cout << "Массив по возрастанию: "<<endl;
+  for (int index = 0; index < n; ++index)
+		{   
+         
+         cout << "a[ " << index+1 << " ] = ";
+         cout<<a[index]<<endl;
+ }
+ cout << "Массив по убыванию: "<<endl;
+  for (int index = n-1; index >=0; --index)
+		{   
+         
+         cout << "a[ " << index+1 << " ] = ";
+         cout<<a[index]<<endl;
+ }
+ delete [] a;
+	return 0;
+}
