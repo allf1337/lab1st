faa907f7f9e3ee68ad571d8a341e99f83798834e
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-12-03 18:21:48 +0300

    Update 4th.cpp

--- lab7th/4th.cpp
+++ lab7th/4th.cpp
@@ -81,7 +81,7 @@ int main()
     for (int i=0;i<size;i++){
         cout<<endl;
         for (int j=0;j<size;j++){
-            a[i][j] = 10 + rand()%89;
+            a[i][j] = rand()%9+1;
             cout<<a[i][j]<<" ";
         }
     }
@@ -156,4 +156,65 @@ int main()
     }
     delete [] b;
     return 0;
+}
+
+
+
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void FillArray(int a[100], int size  ) {
+
+  srand(time(0)); // постоянно рандомные значения массива
+    {
+    int size;
+    cin>>size;
+    int** a=new int*[size];
+    for(int i=0; i<size;i++) {
+        a[i]=new int[size];
+    }
+    int** b=new int*[size];
+    for(int i=0; i<size;i++) {
+        b[i]=new int[size];
+    }
+    for (int i=0;i<size;i++){
+        cout<<endl;
+        for (int j=0;j<size;j++){
+            a[i][j] =  rand()%9+1;
+            cout<<a[i][j]<<" ";
+            delete [] a[i];
+        }
+    }
+  
+  for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            b[i][size-1-j] = a[j][i];
+        }
+    }
+    cout<<endl<<endl<<"Массив повернутый по часовой стрелке: "<<endl;
+    for(int i=0;i<size;i++){
+        cout<<endl;
+        for(int j=0;j<size;j++)
+                cout<<b[i][j]<<" ";
+    }
+    for(int i=0; i<size;i++) {
+        delete [] a[i];
+    }
+    
+    for(int i=0; i<size;i++) {
+        delete [] b[i];
+    }  
+   
+    }
+
+}
+
+int main() {
+
+FillArray(b,size);
+
+
 }
