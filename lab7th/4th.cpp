1c73004672c144a40018f95464fca4e86a402dcb
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-12-04 11:20:00 +0300

    Update 4th.cpp

--- lab7th/4th.cpp
+++ lab7th/4th.cpp
@@ -218,3 +218,54 @@ FillArray(b,size);
 
 
 }
+
+
+
+
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+
+{   
+    srand(time(0));
+    int size;
+    cout << "Введите размер массива: ";
+    cin>>size;
+    int** a=new int*[size];
+    for(int i=0; i<size;i++) {
+        a[i]=new int[size];
+    }
+    int** b=new int*[size];
+    for(int i=0; i<size;i++) {
+        b[i]=new int[size];
+    }
+    for (int i=0;i<size;i++){
+        cout<<endl;
+        for (int j=0;j<size;j++){
+            a[i][j] = 10 + rand()%89;
+            cout<<a[i][j]<<" ";
+        }
+    }
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            b[i][size-1-j] = a[j][i];
+        }
+    }
+    cout<<endl<<endl<<"Массив повернутый по часовой стрелке: "<<endl;
+    for(int i=0;i<size;i++){
+        cout<<endl;
+        for(int j=0;j<size;j++)
+                cout<<b[i][j]<<" ";
+    }
+    for(int i=0; i<size;i++) {
+        delete [] a[i];
+    }
+    delete [] a;
+    for(int i=0; i<size;i++) {
+        delete [] b[i];
+    }
+    delete [] b;
+    return 0;
+}
