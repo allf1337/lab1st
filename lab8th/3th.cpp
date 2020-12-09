942e67983af61b77c09f3aaf1d086e8d3825ca19
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-12-09 21:24:21 +0300

    Update 3th.cpp

--- lab8th/3th.cpp
+++ lab8th/3th.cpp
@@ -79,3 +79,115 @@ osnova(stroka, stolbik, Array, pr);
 del(Array, stroka);
 return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+void Vvod(int str, int stlb, int** arr)
+{
+for (int i = 0; i < str; i++) {
+arr[i] = new int[stlb];
+}
+//Ввод элементов
+ for(int i = 0;i < str; i++)
+    {
+        for(int j = 0;j < stlb;j++)
+        {
+            arr[i][j] = rand()%9+1;
+            cout<<arr[i][j]<<' ';
+        }
+        cout<<endl;
+    }
+
+}
+void osnova(int str, int stol, int** arr, int p){
+  
+
+for (int i = 0; i < str; ++i) {
+//for (int j = 0; j < stol; ++j) 
+{
+  int j;
+  p *= arr[i][j];
+        
+        int k=0;
+        int sum = 0;
+        for (j=0; j<i+1; j++) 
+            sum += arr[j][i-j];
+         k++;
+        
+        cout<<"Сумма элементов параллельных выше побочной диагонали: "<<k<<" = "<<sum<<endl;
+    }
+    int sum = 0;
+    for (int i=0; i<stol; i++)
+    {
+            sum += arr[i][str-i-1];
+    }
+    cout<<"Сумма элементов побочной диагонали: "<<" = "<<sum<<endl;
+    
+    int sum_1=0;
+    
+    for(int i=0; i<str; ++i) {
+        for(int j=0; j<str; ++j) {
+            if (i==j) {
+                sum_1+=arr[i][j];
+            }
+        }
+    }
+    
+     cout<<"Сумма элементов главной диагонали: "<<" = "<<sum_1<<endl;
+    cout<<endl;
+}
+}
+void del(int** arr, int str) {
+
+for (int i = 0; i < str; i++) {
+delete[] arr[i];
+}
+
+delete[] arr;
+}
+
+int main()
+{
+setlocale(LC_ALL, "Russian");
+int sum = 0, pr = 1;
+int stroka, stolbik;
+cout << "Введите кол-во строк" << endl;
+cin >> stroka;
+cout << "Введите кол-во столбиков" << endl;
+cin >> stolbik;
+int** Array = new int* [stroka];
+Vvod(stroka, stolbik, Array);
+osnova(stroka, stolbik, Array, pr);
+del(Array, stroka);
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
