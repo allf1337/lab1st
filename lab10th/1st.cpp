244ff50f9955961727dcafac740b64fc0c272bd6
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-12-10 12:21:13 +0300

    Update 1st.cpp

--- lab10th/1st.cpp
+++ lab10th/1st.cpp
@@ -116,3 +116,130 @@ void OutputGroup(GROUP Group)
   }
 
 // ===================== конец программы =======================
+
+
+
+
+
+
+
+
+
+
+
+
+/***************************************************************
+*                                                             *
+* Файл     : students.cpp                                     *
+*                                                             *
+* Описание : пример программы для обработки структурных типов *
+*            данных STUDENT (Студент) и GROUP (Группа)        *
+*                                                             *
+***************************************************************/
+
+#include <iostream>>
+#include <windows.h>
+#include "pch.h"
+
+// ================ описание типов данных ======================
+
+// ------- описание структурного типа данных АВТОБУС -----------
+struct BUS {
+	int StateNumber;     // гос.номер
+	char Model[15];    // Модель
+	int Year[15];   // Год выпуска
+	int NumberStay; // Мест стоя
+	int NumberSit; // Мест сидя
+};
+
+// ------- описание структурного типа данных ГРУППА ------------
+// максимальное количество студентов в группе
+#define MAX_BUS 30
+struct GROUP {
+	char Name[5];          // Название группы
+	int N;                 // Количество студентов
+	BUS Student[MAX_BUS];  // Массив студентов
+};
+
+// =============== описание прототипов функций =================
+
+// Ввод информации о студенте с клавиатуры
+BUS InputBUS(void);
+
+// Вывод информации о студенте на экран
+void OutputBUS(BUS BUS);
+
+// Ввод информации о группе с клавиатуры
+//void InputGroup(GROUP *Group);
+
+// Вывод информации о группе на экран
+//void OutputGroup(GROUP Group);
+
+// =============== основная функция программы ==================
+
+int main(int argc, char* argv[])
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	GROUP Group;
+	InputGroup(&Group);
+	OutputBus(Group);
+
+	Sleep(7654);
+	return 0;
+}
+
+// ============ Ввод информации о студенте с клавиатуры ========
+BUS InputStudent(void)
+{
+	BUS bus;
+	cout << "Гос. номер ====> ";
+	cin >> bus.StateNumber;
+	cout << "Модель ========> ";                                       
+	cin >> bus.Model;
+	cout << "Год ===> ";
+	cin >> bus.Year;
+	cout << "Мест стоя ===> ";
+	cin >> bus.NumberStay;
+	cout << "Мест сидя ===> ";
+	cin >> bus.NumberSit;
+
+	return bus;
+}
+
+// ============ Вывод информации о студенте на экран ===========
+void OutputBus(BUS Bus)
+{
+	cout.width(8);
+	cout.fill('0');
+	cout << Bus.StateNumber << " "
+		<< Bus.Model << " "
+		<< Bus.Year[0] << ". "
+		<< Bus.NumberSit[0] << ". "
+		<< Bus.NumberStay[0] << ".\n";
+}
+
+// ============ Ввод информации о группе с клавиатуры ==========
+void InputGroup(GROUP *Group)
+{
+	cout << "Введите название группы ===> ";
+	cin >> Group->Name;
+	cout << "Введите количество студентов (не более "
+		<< MAX_BUS << ") ===> ";
+	cin >> Group->N;
+	for (int i = 0; i < Group->N; i++)
+	{
+		cout << "Введите информацию о " << (i + 1) << "-ом студенте\n";
+		Group->Bus[i] = InputBUS();
+	}
+}
+
+// ============ Вывод информации о группе на экран =============
+//void OutputGroup(GROUP Group)
+//{
+	//cout << "Список группы " << Group.Name << "\n";
+//	for (int i = 0; i < Group.N; i++) OutputBus(Group.Bus[i]);
+//}
+
+// ===================== конец программы ==
