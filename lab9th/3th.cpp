558a64f5c9e1c8dede22693b1038a94a379b2b63
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-12-10 11:51:48 +0300

    Update 3th.cpp

--- lab9th/3th.cpp
+++ lab9th/3th.cpp
@@ -1,1 +1,46 @@
+1)
+  
+  
+  
+  
+  2)
+#include <iostream>
+#include <cstring>
+#include <clocale>
+using namespace std;
 
+bool polindrom(string word)
+{
+  int *del;
+  del=new int;
+	int len = word.length();
+	for(int i = 0; i < len/2; ++i)
+	{
+		if(word[i] != word[len-i-1])
+		{
+			return false;
+		}
+	}
+	return true;
+  delete del;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+  int *del;
+  del=new int;
+  string word;
+	cout << "Введите слово: ";
+	cin >> word;
+	if(polindrom(word))
+	{
+		cout << "Слово - полиндром.";
+	}
+	else
+	{
+		cout << "Слово - не полидром";
+	}
+  delete del;
+	return 0;
+}
