26fa6c2835b81011d1593ba9b306a0bbb88cea08
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-12-10 08:26:53 +0300

    Update 4th.cpp

--- lab7th/4th.cpp
+++ lab7th/4th.cpp
@@ -1,17 +1,9 @@
 
-
-
-
-
-
 #include <iostream>
 using namespace std;
  
-int main()
-{
-    int size;
-    cin>>size;
-    int** a=new int*[size];
+void FillArray(int size){
+int** a=new int*[size];
     for(int i=0; i<size;i++) {
         a[i]=new int[size];
     }
@@ -22,7 +14,7 @@ int main()
     for (int i=0;i<size;i++){
         cout<<endl;
         for (int j=0;j<size;j++){
-            a[i][j] = rand()%9+1;
+            a[i][j] = 10 + rand()%89;
             cout<<a[i][j]<<" ";
         }
     }
@@ -31,13 +23,16 @@ int main()
             b[i][size-1-j] = a[j][i];
         }
     }
-    cout<<endl<<"++++++++";
+    cout<<endl<<endl<<"Массив повернутый по часовой стрелке: "<<endl;
     for(int i=0;i<size;i++){
         cout<<endl;
         for(int j=0;j<size;j++)
                 cout<<b[i][j]<<" ";
     }
-    for(int i=0; i<size;i++) {
+
+}
+void delArray(int** a, int size, int** b ){
+  for(int i=0; i<size;i++) {
         delete [] a[i];
     }
     delete [] a;
@@ -45,9 +40,22 @@ int main()
         delete [] b[i];
     }
     delete [] b;
-    return 0;
 }
 
+int main()
+
+{   
+    srand(time(0));
+    int size;
+    cout << "Введите размер массива: ";
+    cin>>size;
+    int** a=new int*[size];
+    int** b=new int*[size];
+    FillArray(size);
+    delArray ();
+   
+    return 0;
+}
 
 
 
