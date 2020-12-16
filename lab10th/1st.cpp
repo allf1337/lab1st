dda13cb3ad79a4a8e50874125b94a50e9cac1a6e
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-12-16 12:56:18 +0300

    Update 1st.cpp

--- lab10th/1st.cpp
+++ lab10th/1st.cpp
@@ -395,4 +395,161 @@ void APersons(Persdepart bS)
 
 
 
+//#include <windows.h>
+//#include "pch.h"
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+
+struct Person {
+	int NumberState;     
+	char Model[15];  
+	int NumberStay;   
+
+
+	
+	int NumberSit; 
+	int year; 
+	int NumberAll;  
+ 
+};
+
+struct Persdepart {
+
+	int N;                 
+	Person *persons;  
+};
+
+Person InputPerson(void);
+
+void OutputPerson(Person person);
+
+void InputPersDepart(Persdepart *depart);
+
+void OutputPersDepart(Persdepart depart);
+
+void SortPersons(Persdepart bS);
+
+void APersons(Persdepart bS);
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	
+	
+	Persdepart persdepart;
+
+
+	InputPersDepart(&persdepart);
+	OutputPersDepart(persdepart);
+
+	APersons(persdepart);
+	system("pause");
+	return 0;
+}
+
+Person InputPerson(void)
+{
+	Person person;
+	cout << "State Number ====> ";
+	cin >> person.NumberState;
+
+	cout << "Model ========> ";
+	cin >> person.Model;
+
+	cout << "NumberStay ===> ";
+	cin >> person.NumberStay;
+
+	
+
+	cout << "NumberSit ===> ";
+	cin >> person.NumberSit;
+
+	cout << "Year ===> ";
+	cin >> person.year;
+
+	
+	person.NumberAll = person.NumberSit + person.NumberStay;
+
+	cout << "----------------------------------------------------------" << endl;
+	return person;
+}
+
+
+void OutputPerson(Person person)
+{
+	cout.width(8);
+	cout.fill('0');
+
+	cout << "NumberState:" << person.NumberState << ", "
+		<< "Model:" << person.Model << ", "
+		<< "NumberStay:" << person.NumberStay << ", "
+		//<< "Sex:" << person.sex << ". "
+		<< "NumberSit:" << person.NumberSit << ". "
+		<< "Year:" << person.year << ". "
+		<< "NumberAll:" << person.NumberAll << ".\n ";
+}
+
+
+
+void InputPersDepart(Persdepart *depart)
+{
+
+	cout << "Enter number of bus: " << endl;
+	cin >> depart->N;
+	depart->persons = new Person[depart->N];
+
+	for (int i = 0; i < depart->N; ++i)
+		depart->persons[i] = InputPerson();
+}
+
+// ============ Вывод информации оо отделе кадров   с клавиатуры ==========
+void OutputPersDepart(Persdepart depart)
+{
+	//cout << "Список работников пенсионного возраста: " << endl;
+	SortPersons(depart);
+	for (int i = 0; i < depart.N; ++i)
+		OutputPerson(depart.persons[i]);
+}
+
+// Сортировка персонала
+void SortPersons(Persdepart bS)
+{
+	for (int i = 1; i < bS.N; ++i)
+	{
+		for (int c = i; c > 0; --c)
+		{
+			if (bS.persons[c].year > bS.persons[c - 1].year)
+				swap(bS.persons[c], bS.persons[c - 1]);
+		}
+	}
+}
+
+
+// Вывод работников пенсионного возраста
+void APersons(Persdepart bS)
+{
+	int G;
+  cout << "NumberAll : " << endl;
+  cin >> G;
+	for (int i = 0; i < bS.N; ++i)
+	{
+		if (bS.persons[i].NumberAll >= G) {
+			cout << "NumberState: "<< bS.persons[i].NumberState ;
+			cout << " Model: "<< bS.persons[i].Model ;
+			cout << " Year: "<<bS.persons[i].year << endl;
+
+
+
+
+
+		}
+
+	}
+
+}
+
+
 
