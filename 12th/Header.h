74af1778c7478c3f8e4454c12b725e8994f32752
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-12-16 20:31:17 +0300

    Create Header.h

---
+++ 12th/Header.h
@@ -0,0 +1,21 @@
+
+
+
+struct Person {
+	int NumberState;
+	char Model[15];
+	int NumberStay;
+
+
+
+	int NumberSit;
+	int year;
+	int NumberAll;
+
+};
+
+struct Persdepart {
+
+	int N;
+	Person* persons;
+};
