53be1af76ed0fd65ae97d354814bc563d0b4a740
allf1337 <70582258+allf1337@users.noreply.github.com>
2020-12-21 12:32:52 +0300

    Create iostudent.h

---
+++ 121/12.1/iostudent.h
@@ -0,0 +1,37 @@
+/***************************************************************
+ *                                                             *
+ * Файл     : iostudent.h                                      *
+ *                                                             *
+ * Описание : заголовочный файл, описывающий функции           *
+ *            для организации потокового ввода-вывода          *
+ *            переменных структурных типов данных              *
+ *            STUDENT (Студент) и GROUP (Группа)               *
+ *                                                             *
+ ***************************************************************/
+
+#ifndef IOSTUDENT_H
+#define IOSTUDENT_H
+
+#include <iostream>
+#include "students.h"
+#include <fstream>
+
+using namespace std;
+
+// Ввод информации о студенте из входного потока
+STUDENT ReadStudent(istream &in);
+
+// Вывод информации о студенте в выходной поток
+void WriteStudent(ostream &out, STUDENT Student);
+
+// Ввод информации о группе из входного потока
+void ReadGroup(istream &in, GROUP *Group);
+
+// Вывод информации о группе в выходной поток
+void WriteGroup(ostream &out, GROUP Group);
+
+
+
+#endif
+
+// =============== конец заголовочного файла  ==================
