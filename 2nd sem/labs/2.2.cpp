12f598f2814080c15927603b780c6b61d384badc
allf1337 <70582258+allf1337@users.noreply.github.com>
2021-03-10 13:51:55 +0300

    Update 2.2.cpp

--- 2nd sem/labs/2.2.cpp
+++ 2nd sem/labs/2.2.cpp
@@ -1,1 +1,23 @@
+#include "stdafx.h"
+#include <iostream>
+#include <locale.h>
 
+using namespace std;
+
+int main()
+{
+
+	setlocale(LC_ALL, "Russian");
+	int a,b;
+	cout << "Введите число a: ";
+	cin >> a;
+	cout << "Введите число b: ";
+	cin >> b;
+	if (a % b == 0 ||  b % a == 0) 
+		cout << "Да, одно из чисел является делителем другого" << endl;
+	
+	else 
+		cout << "Нет, ни одно из чисел не является делителем другого" << endl;
+	
+	return 0;
+}
