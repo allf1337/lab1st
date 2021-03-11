20bf6387fa6c06b13a957b9b7441410e0295f0a7
allf1337 <70582258+allf1337@users.noreply.github.com>
2021-03-11 14:14:06 +0300

    Update 1.1.cpp

--- 2nd sem/labs/proga/1.1.cpp
+++ 2nd sem/labs/proga/1.1.cpp
@@ -1,37 +1,24 @@
-#include <string>
 #include <iostream>
-#include <locale.h>
-
+#include <string>
 using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    int count_s1, count_s2, n1, n2, aints;
-    char s1[100]; 
-    char s2[100];
-    char s3[100];
-    cout << "Введите s1: ";
-    cin.getline(s1, sizeof(s1));
-    count_s1 = strlen(s1);
-    cout << "Введите s2: ";
-    cin.getline(s2, sizeof(s2));
-    count_s2 = strlen(s2);
-    cout << "Введите n1: ";
-    cin >> n1;
-    //cout << count_s1;
-    string s(s3);
-  
-    cout << "Введите n2: ";
-    cin >> n2;
-    //cout << count_s1;
+    int N1, N2;
+    cout << "N1: "; cin >> N1;
+    cout << "N2: "; cin >> N2;
 
-    //string s1; 
-    cout << s.append(s1, 1, n1);
-   
-    //cout << aints;
-   
+    cin.ignore();
+    string S1, S2, S3;
+    cout << "S1: ";
+    getline(cin, S1);
+    cout << "S2: ";
+    getline(cin, S2);
 
+    S3 = S1.substr(0, N1) + S2.substr(S2.size() - N2);
+
+    cout << S3 << "\n";
+
+    system("pause");
     return 0;
 }
-
